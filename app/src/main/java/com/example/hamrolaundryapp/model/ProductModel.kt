81b87617c358bd90e47b1b4ca61d30522ddcd4bf
sujatalimbu2 <tolilimbu2@gmail.com>
2026-06-07 10:10:28 +0545

    changes in androidmanis , buid

---
+++ app/src/main/java/com/example/hamrolaundryapp/model/ProductModel.kt
@@ -0,0 +1,8 @@
+package com.example.hamrolaundryapp.model
+
+data class ProductModel(
+    var id: String = "",
+    val name: String = "",
+    val description: String = "",
+    val price: Double = 0.0
+)
